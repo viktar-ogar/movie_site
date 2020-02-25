a1c2ec02e776653d964365d81bec1338b4cf62c7
Viktor <kebertv@ya.ru>
2020-02-25 22:50:41 +0600

    Add deploy

--- movie_site/settings.py
+++ movie_site/settings.py
@@ -124,10 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
+# STATIC_DIR = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [STATIC_DIR]
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
