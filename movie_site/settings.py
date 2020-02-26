90100d59a80673912185d94f536bc462d0cbbb8d
Viktor <kebertv@ya.ru>
2020-02-26 21:44:10 +0600

    Add fjango-heroku

--- movie_site/settings.py
+++ movie_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,3 +136,5 @@ STATICFILES_DIRS = [
 # Media folder and url
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
