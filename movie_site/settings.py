dae440b45d7c5d994e62e289c576705631d009fa
Viktor <kebertv@ya.ru>
2020-02-27 15:21:44 +0600

    Deploy wsgi

--- movie_site/settings.py
+++ movie_site/settings.py
@@ -129,8 +129,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
     ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media folder and url
 MEDIA_URL = '/media/'
