17a89ecf000524b2fe9c2546c0b17a79ce597454
Viktor <kebertv@ya.ru>
2020-02-28 21:33:42 +0600

    Add ckeditor, youtube addon

--- movie_site/settings.py
+++ movie_site/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', ')z3v^c-udo15gj8kwflh+s=zszxpyjw$7^*0ys2!)a@m*fhhng')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
+#DEBUG = False
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['movie-django-market.herokuapp.com', '127.0.0.1']
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'ckeditor',
+    'ckeditor_uploader',
     'movies',
 ]
 
@@ -51,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
@@ -110,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -139,3 +142,75 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Media folder and url
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# CKeditor - Исходный код: https://github.com/django-ckeditor/django-ckeditor
+CKEDITOR_UPLOAD_PATH = 'uploads/'
+
+CKEDITOR_CONFIGS = {
+    'default': {
+        'skin': 'moono',
+        # 'skin': 'office2013',
+        'toolbar_Basic': [
+            ['Source', '-', 'Bold', 'Italic']
+        ],
+        'toolbar_YourCustomToolbarConfig': [
+            {'name': 'document', 'items': ['Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates']},
+            #{'name': 'clipboard', 'items': ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo']},
+            {'name': 'editing', 'items': ['Find', 'Replace', '-', 'SelectAll']},
+            {'name': 'forms',
+             'items': ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton',
+                       'HiddenField']},
+            #'/',
+            {'name': 'basicstyles',
+             'items': ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']},
+            {'name': 'colors', 'items': ['TextColor', 'BGColor']},
+            {'name': 'tools', 'items': ['Maximize', 'ShowBlocks']},
+            {'name': 'about', 'items': ['About']},
+             '/',
+            {'name': 'paragraph',
+             'items': ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-',
+                       'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl',
+                       'Language']},
+            {'name': 'links', 'items': ['Link', 'Unlink', 'Anchor']},
+            {'name': 'insert',
+             'items': ['Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe']},
+            '/',
+            {'name': 'styles', 'items': ['Styles', 'Format', 'Font', 'FontSize']},
+            #'/',  # put this to force next toolbar on new line
+            {'name': 'yourcustomtools', 'items': [
+                # put the name of your editor.ui.addButton here
+                'Preview',
+                'Maximize',
+                'Youtube',
+
+            ]},
+        ],
+        'toolbar': 'YourCustomToolbarConfig',  # put selected toolbar config here
+        # 'toolbarGroups': [{ 'name': 'document', 'groups': [ 'mode', 'document', 'doctools' ] }],
+        # 'height': 291,
+        # 'width': '100%',
+        # 'filebrowserWindowHeight': 725,
+        # 'filebrowserWindowWidth': 940,
+        # 'toolbarCanCollapse': True,
+        # 'mathJaxLib': '//cdn.mathjax.org/mathjax/2.2-latest/MathJax.js?config=TeX-AMS_HTML',
+        'tabSpaces': 4,
+        'extraPlugins': ','.join([
+            'uploadimage', # the upload image feature
+            # your extra plugins here
+            'div',
+            'autolink',
+            'autoembed',
+            'embedsemantic',
+            'autogrow',
+            # 'devtools',
+            'widget',
+            'lineutils',
+            'clipboard',
+            'dialog',
+            'dialogui',
+            'elementspath',
+            'youtube'
+        ]),
+    }
+}
--- movies/admin.py
+++ movies/admin.py
@@ -1,12 +1,155 @@
 from django.contrib import admin
 from .models import Category, Actor, Genre, Movie, MovieShots, RatingStar, Rating, Reviews
+from django.utils.safestring import mark_safe
 
-admin.site.register(Category)
-admin.site.register(Actor)
-admin.site.register(Genre)
-admin.site.register(Movie)
-admin.site.register(MovieShots)
+# CKUploader Widjet
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class MovieAdminForm(forms.ModelForm):
+    description = forms.CharField(label="Описание", widget=CKEditorUploadingWidget())
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    
+    ''' Категории фильмов '''
+
+    list_display = ("id", "name", "url",)
+    list_display_links = ("name", )
+
+
+class ReviewsInLine(admin.TabularInline):
+    
+    ''' Отзывы на странице фильма '''
+
+    model = Reviews
+    extra = 1
+    readonly_fields = ("name", "email")
+
+
+class MovieShotsInLine(admin.TabularInline):
+
+    ''' Кадры из фильма на странице фильма '''
+
+    model = MovieShots
+    extra = 1
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="110" height="100">')
+
+    get_image.short_description = "Изображение"
+
+
+class MovieAdmin(admin.ModelAdmin):
+    
+    ''' Фильмы '''
+
+    list_display = ("title", "category", "url", "draft")
+    list_filter = ("category", "year")
+    search_fields = ("title", "category__name")
+    inlines = [MovieShotsInLine, ReviewsInLine]
+    save_on_top = True
+    #save_as = True  // Добавляет вместо "Сохранить и добавить новый объект" ---> "Сохранить как новый объект"
+    list_editable = ("draft", )
+    form = MovieAdminForm
+    readonly_fields = ("get_image", )
+    fieldsets = (
+        (None, {
+            'fields': ("title", "tagline")
+        }),
+        (None, {
+            'fields': ("description", ("poster", "get_image"))
+        }),
+        (None, {
+            'fields': (("year", "world_premier", "country"), )
+        }),
+        (None, {
+            'fields': (("genres", "category"), )
+        }),
+        ("Режиссеры и актеры", {
+            'classes': ("collapse",), # Скрытие названия группы
+            'fields': (("directors", "actors"), )
+        }),
+        (None, {
+            'fields': (("budget", "fees_in_russia", "fees_in_world"), )
+        }),
+        ("Опции", {
+            'fields': (("url", "draft"), )
+        }),
+    )
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.poster.url} width="90" height="110">')
+
+    get_image.short_description = "Изображение постера"
+
+
+class ReviewsAdmin(admin.ModelAdmin):
+    
+    ''' Отзывы '''
+
+    list_display = ("name", "email", "parent", "movie", "id")
+    readonly_fields = ("name", "email")
+
+
+class ActorAdmin(admin.ModelAdmin):
+    
+    ''' Режиссеры и актеры '''
+
+    list_display = ("name", "age", "get_image")
+    list_display_links = ("name", )
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60">')
+        
+    get_image.short_description = "Изображение"
+
+
+class GenreAdmin(admin.ModelAdmin):
+    
+    ''' Жанры '''
+
+    list_display = ("name", "url")
+    list_display_links = ("name", )
+
+
+class RatingAdmin(admin.ModelAdmin):
+    
+    ''' Рейтинги '''
+
+    list_display = ("movie", "ip")
+    list_display_links = ("movie", )
+
+
+class MovieShotsAdmin(admin.ModelAdmin):
+
+    ''' Кадры из фильмов '''
+
+    list_display = ("title", "movie", "get_image")
+    readonly_fields = ("get_image", )
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="60" height="50">')
+
+    get_image.short_description = "Кадры из фильма"
+
+
+# admin.site.register(Category, CategoryAdmin)
+admin.site.register(Actor, ActorAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Rating, RatingAdmin)
+admin.site.register(Reviews, ReviewsAdmin)
+admin.site.register(MovieShots, MovieShotsAdmin)
 admin.site.register(RatingStar)
-admin.site.register(Rating)
-admin.site.register(Reviews)
 
+# Change site title
+admin.site.site_title = "Кино Маркет Django"
+admin.site.site_header = "Кино Маркет Django"
